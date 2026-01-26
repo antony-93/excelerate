4218f6e9236023d118dfd74bf80f14dda372b89d
antony.cechinel <antony.cechinel@useall.com.br>
2026-01-26 18:40:29 -0300

    feat/Added reload classes in hmr

--- src/presentation/public/assets/hmr.js
+++ src/presentation/public/assets/hmr.js
@@ -1,9 +1,94 @@
-class ExcelerateHmr {
+const reloadTypes = {
+    hot: 'hot',
+    live: 'live'
+}
+
+class ExcelerateClient {
+    #webSocket
+    #liveReloadHandler
+    #hotReloadHandler
+
+    constructor() {
+        this.#webSocket = new ExcelerateWebSocket();
+        this.#liveReloadHandler = new ExcelerateLiveReloadHandler();
+        this.#hotReloadHandler = new ExcelerateHotReloadHandler();
+    }
+
     initialize() {
-        console.log('DEU BOM');
+        this.initWebSocket();
+    }
+
+    initWebSocket() {
+        this.#webSocket.connect();
+
+        this.#webSocket.onMessage(({ type, ...props }) => {
+            const reload = type === reloadTypes.hot
+                ? this.#hotReloadHandler 
+                : this.#liveReloadHandler;
+                
+            reload.handle(...props);
+        });
+    }
+}
+class ExcelerateWebSocket {
+    #socketUrl
+    #webSocket
+
+    constructor(urlConnection) {
+        this.#socketUrl = urlConnection;
+    }
+
+    connect() {
+        this.#webSocket = new WebSocket(this.#socketUrl);
+        this.#config()
+    }
+
+    onMessage(cb) {
+        this.#webSocket.onmessage = (event) => {
+            try {
+                const message = JSON.parse(event.data);
+                cb(message);
+            } catch (err) {
+                console.error('❌ [Excelerate] Erro ao processar mensagem:', err);
+            }
+        };
+    }
+
+    #config() {
+        this.#webSocket.onclose = () => {
+            console.warn('⚠️ [Excelerate] Conexão perdida. Tentando reconectar em 3s...');
+            setTimeout(() => this.connect(), 3000);
+        };
+
+        this.#webSocket.onerror = (error) => {
+            console.error('❌ [Excelerate] Erro no WebSocket');
+        };
     }
 }
 
-const execelerateHmr = new ExcelerateHmr();
+class ExcelerateLiveReloadHandler {
+    handle() {
+        window.location.reload();
+    }
+}
 
-execelerateHmr.initialize();
+class ExcelerateHotReloadHandler {
+    handle({ path }) {
+        const canHotReload = this.#canHotReload();
+    
+        if (!canHotReload) return;
+
+    }
+
+    #canHotReload() {
+        return true;
+    }
+
+    #reloadClasses() {
+        
+    }
+}
+
+
+const excelerate = new ExcelerateClient();
+excelerate.initialize();
